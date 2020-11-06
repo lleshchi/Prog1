0147e08c44c5b0592e5a6cca40f85c90b50bcb89
Leah Leshchinsky <Leah_Leshchinsky@student.uml.edu>
2020-11-05 22:15:07 -0500

    Additions
    Signed-off-by: Leah Leshchinsky <Leah_Leshchinsky@student.uml.edu>

--- client.py
+++ client.py
@@ -31,43 +31,49 @@ def parser():
     return my_parser.parse_args()
 
 def main():
-
+    client_socket = None
+    file = None
     args = parser()
 
     if args.Path[0] is not "/":
         args.Path = "/" + args.Path
     my_request = Request(args.Name, int(args.Port), args.Method, args.Path)
 
-    # socket() call returns socket object with available methods that translate to socket system calls
-    # AF_INET refers to the Internet Protocol v4 address family that the socket will communicate with
-    # SOCK STREAM defines the socket as a TCP socket. SOCK_DGRAM can be used for UDP
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((my_request.name, my_request.port))
     try:
+        # socket() call returns socket object with available methods that translate to socket system calls
+        # AF_INET refers to the Internet Protocol v4 address family that the socket will communicate with
+        # SOCK STREAM defines the socket as a TCP socket. SOCK_DGRAM can be used for UDP
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((my_request.name, my_request.port))
+
         # GET /index.html HTTP/1.1
         if my_request.method == "GET":
             msg = my_request.method + " " + my_request.path + " " + "HTTP/1.1\r\nHost:" + my_request.name + "\r\n\r\n"
             client_socket.sendall(msg.encode())
             client_socket.shutdown(1)   # Good etiquette, let server know client is done sending but still receiving
+
         if my_request.method == "PUT":
             msg = my_request.method + " " + my_request.path + "\r\n\r\n"
             client_socket.sendall(msg.encode())
             file = open("." + my_request.path, "rb")
-            client_socket.send(file.read())
+            data = file.read()
+            while data:
+                client_socket.send(data)
+                data = file.read()
             client_socket.shutdown(1)  # Good etiquette, let server know client is done sending but still receiving
             file.close()
+
         server_response = client_socket.recv(4096)
         while server_response:
             print(server_response.decode() + "\n")
             server_response = client_socket.recv(4096)  # Must be called until no data is left (returns 0 bytes)
 
-        client_socket.close()
-        print("\nClosing Client Socket\n")
-
-    except Exception as e:
-        client_socket.close()
-        print("\nClosing Client Socket\n")
-        raise e
+    finally:
+        if file:
+            file.close()
+        if client_socket:
+            client_socket.close()
+            print("\nClosing Client Socket\n")
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import argparse
+import os
 
 def parser():
     my_parser = argparse.ArgumentParser()
@@ -14,67 +15,80 @@ def parser():
 
 
 def main():
-
+    server_socket = None
     args = parser()
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
+        # create socket object
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         # IP address and port number assigned to socket instance
         # The '' means that this server will by reachable by any address this machine has (same as 0.0.0.0)
-        server_socket.bind(("0.0.0.0", int(args.Port)))
+        server_socket.bind(('0.0.0.0', int(args.Port)))
+
         # 1 is the backlog size, allows 1 unaccepted connection before it begins to refuse new connections
         server_socket.listen(1)
 
         while True:
-
-            # conn_socket is new socket object. Use it to send data to client
-            # addr is the address on the other side of the connection
-            conn_socket, addr = server_socket.accept()
+            conn_socket = None
             try:
+                # conn_socket is new socket object. Use it to send data to client
+                # addr is the address on the other side of the connection
+                conn_socket, addr = server_socket.accept()
+
+                # recv message, max buffer size 1024
                 recv_message = conn_socket.recv(1024).decode()
+
+                # split into list words by dividing on whitespace
+                # First word will be method, second will be file path
                 recv_list = recv_message.split()
                 method = recv_list[0]
                 path = recv_list[1]
+
                 if method == "GET":
                     try:
+                        # Look in current directory (I know I know, there's a better way to do this)
+                        # Open file with that name for reading
+                        # Send 200 OK confirmation to client (make sure to encode it from string to bytes)
+
                         path = "." + path
                         file = open(path, "rb")
                         msg = "HTTP/1.1 200 OK\r\n"
                         conn_socket.send(msg.encode())
                         msg = "Content-Type: text/html\r\n"
                         conn_socket.send(msg.encode())
-                        conn_socket.send(file.read())
-                        file.close()
+                        data = file.read()
+                        while data:
+                            conn_socket.send(data)
+                            data = file.read()
 
-                    except FileNotFoundError:
-                        msg = "404 Not Found\r\n"
+                    except (FileNotFoundError, IsADirectoryError):
+                        msg = "HTTP/1.1 404 Not Found\r\n\r\n Sorry, your file request was not found :("
                         conn_socket.send(msg.encode())
 
-                    print("\nClosing Connection Socket\n")
-                    conn_socket.close()
                 if method == "PUT":
-
                     print(path)
-                    file = open(r"tmp" + path, "wb")
-                    data = conn_socket.recv(2048)
-                    file.write(data)
-                    file.close()
+                    if not os.path.exists("tmp/"):
+                        os.makedirs("tmp/")
+                    with open("tmp" + path, "wb+") as file:
+                        data = conn_socket.recv(4096)
+                        while data:
+                            file.write(data)
+                            data = conn_socket.recv(4096)
                     msg = "200 OK File Created\r\n"
                     conn_socket.send(msg.encode())
 
-                    print("\nClosing Connection Socket\n")
-                    conn_socket.close()
-
-            except BaseException as e:
+            finally:
                 if conn_socket:
                     print("\nClosing Connection Socket\n")
                     conn_socket.close()
-                raise e
 
-    except BaseException:
+    except KeyboardInterrupt:
+        exit()
+
+    finally:
         if server_socket:
             print("\nClosing Server Socket\n")
             server_socket.close()
-        exit()
 
 
 if __name__ == "__main__":
